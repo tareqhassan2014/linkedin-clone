98d5ab137471cd83bfb192742dccbdf0388a1312
Tareq Hassan <tareqhassan2014@gmail.com>
2021-05-13 00:12:47 +0600

    remove everyting from readme

--- src/components/Longin.js
+++ src/components/Longin.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Longin = () => {
     return (
-        <div>
-            <h1>Lognin Page.</h1>
-        </div>
+       
+               <img src="/images/login-logo.svg" alt="" />
+      
     );
 };
+
+
+
 
 export default Longin;
